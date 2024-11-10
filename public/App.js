89a08f01b1a8f3d0c373dfce7a98320f16ebab3e
Portfolio of Projects by Alexis <info@alexiscollier.com>
2024-11-09 22:14:26 -0500

    Add files via upload

---
+++ public/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div>
+            <h1>RealityShift: Transforming Real-World Learning Through XR</h1>
+            <p>An immersive app that enhances training in fields like medicine, engineering, and the arts through VR and AR simulations.</p>
+        </div>
+    );
+}
+
+export default App;
